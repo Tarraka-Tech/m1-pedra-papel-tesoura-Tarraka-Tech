a1068c9b56852354b2b7d75381f3701315e4dc78
Tarraka-Tech <renangarciagomes@gmail.com>
2024-02-15 14:08:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2){
+
+    const partida = player1 && player2
+
+    if ((partida === "Pedra","Tesoura")||(partida === "Papel","Pedra")||(partida === "Tesoura","Papel")){
+        return "Jogador 1 venceu!"
+    }
+
+     else if ((partida === "Pedra","Pedra")||(partida === "Tesoura","Tesoura")||(partida === "Papel","Papel")){
+        return "Empate!"
+    }
+
+     else if ((partida === "Tesoura","Pedra")||(partida === "Pedra","Papel")||(partida === "Papel","Tesoura")){
+        return "Jogador 2 venceu!"
+    }
+}
