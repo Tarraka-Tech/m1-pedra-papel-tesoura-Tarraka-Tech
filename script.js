3690c2bcd9f374d693a92f908ff9630661944e86
Tarraka-Tech <renangarciagomes@gmail.com>
2024-02-15 20:36:39 -0300

    Atualização da lógica
    Atualização da lógica e correção do uso das {}

--- script.js
+++ script.js
@@ -6,38 +6,28 @@ function playRockPaperScissor (player1, player2){
     const Papel = "Papel"
     const Tesoura = "Tesoura"
 
-    if (player1 == Pedra && player2 == Tesoura){
+    if ((player1 == Pedra && player2 == Tesoura) ||
+    
+    (player1 == Tesoura && player2 == Papel) ||
+    
+    (player1 == Papel && player2 == Pedra)){
+       
         return "Jogador 1 venceu!"
-    }
-
-    else if (player1 == Tesoura && player2 == Papel){
-        return "Jogador 1 venceu!"
-    }
-
-    else if (player1 == Papel && player2 == Pedra){
-        return "Jogador 1 venceu!"
-    }
-
-    else if (player1 == Pedra && player2 == Pedra){
+    
+    }else if ((player1 == Pedra && player2 == Pedra) ||
+    
+    (player1 == Papel && player2 == Papel) ||
+    
+    (player1 == Tesoura && player2 == Tesoura)){
+        
         return "Empate!"
-    }
-
-    else if (player1 == Papel && player2 == Papel){
-        return "Empate!"
-    }
-
-    else if (player1 == Tesoura && player2 == Tesoura){
-        return "Empate!"
-    }
-
-    else if (player1 == Pedra && player2 == Papel){
+    
+    }else if ((player1 == Pedra && player2 == Papel) ||
+    
+    (player1 == Papel && player2 == Tesoura) ||
+    
+    (player1 == Tesoura && player2 == Pedra)){
+        
         return "Jogador 2 venceu!"
     }
-
-    else if (player1 == Papel && player2 == Tesoura){
-        return "Jogador 2 venceu!"
-    }
-
-    else (player1 == Tesoura && player2 == Pedra)
-        return "Jogador 2 venceu!"
 }
